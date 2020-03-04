609af0c0c9f2adf68766f08465624e20be416e22
Martin Valchev <mv1u18@soton.ac.uk>
2020-03-04 02:04:12 +0000

    added simple logging

--- src/main/java/group18/dashboard/controllers/AppController.java
+++ src/main/java/group18/dashboard/controllers/AppController.java
@@ -197,7 +197,6 @@ public class AppController {
             }
             updateMetrics(executor);
             updateChartMetrics(executor, Calendar.DAY_OF_MONTH);
-            System.out.println("--Finished Parsing Campaign--");
         });
         executor.shutdown();
         chartPane.getChildren().remove(importNotification);
@@ -205,6 +204,7 @@ public class AppController {
     }
 
     private void updateMetrics(ExecutorService executor) {
+        System.out.println("--Calculating Metrics--");
         executor.execute(this::updateBounceRate);
         executor.execute(this::updateBounces);
         executor.execute(this::updateClickCount);
@@ -221,63 +221,74 @@ public class AppController {
     private void updateBounceRate() {
         var bounceRate = ViewDataParser.getBounceRate(in.getClicks(), in.getInteractions());
         Platform.runLater(() -> in.setBounceRate(bounceRate));
+        System.out.println("bounce rate... done");
     }
 
     private void updateBounces() {
         var bounces = ViewDataParser.getBounces(in.getInteractions());
         Platform.runLater(() -> in.setBounces(bounces));
+        System.out.println("bounces... done");
     }
 
     private void updateClickCount() {
         var clicks = in.getClicks().size();
         Platform.runLater(() -> in.setClickCount(clicks));
+        System.out.println("clicks... done");
     }
 
     private void updateConversions() {
         var conversions = ViewDataParser.getConversions(in.getInteractions());
         Platform.runLater(() -> in.setConversions(conversions));
+        System.out.println("conversions... done");
     }
 
     private void updateCPA() {
         var cpa = ViewDataParser.getCPA(in.getImpressions(), in.getClicks(), in.getInteractions());
         Platform.runLater(() -> in.setCpa(cpa));
+        System.out.println("CPA... done");
     }
 
     private void updateCPC() {
         var cpc = ViewDataParser.getCPC(in.getImpressions(), in.getClicks());
         Platform.runLater(() -> in.setCpc(cpc));
+        System.out.println("CPC... done");
     }
 
     private void updateCPM() {
         var cpm = ViewDataParser.getCPM(in.getImpressions(), in.getClicks());
         Platform.runLater(() -> in.setCpm(cpm));
+        System.out.println("CPM... done");
     }
 
     private void updateCTR() {
         var ctr = ViewDataParser.getCTR(in.getImpressions(), in.getClicks());
         Platform.runLater(() -> in.setCtr(ctr));
+        System.out.println("CTR... done");
     }
 
     private void updateImpressions() {
         var impressions = in.getImpressions().size();
         Platform.runLater(() -> in.setImpressionCount(impressions));
+        System.out.println("impressions... done");
     }
 
     private void updateTotalCost() {
         var totalCost = ViewDataParser.getTotalCost(in.getImpressions(), in.getClicks());
         Platform.runLater(() -> in.setTotalCost(totalCost));
+        System.out.println("total cost... done");
     }
 
     private void updateUniques() {
         var uniques = ViewDataParser.getUniques(in.getClicks());
         Platform.runLater(() -> in.setUniques(uniques));
+        System.out.println("uniques... done");
     }
 
     private void updateChartMetrics(ExecutorService executor, int resolution) {
         executor.execute(() -> updateBounceRateSeries(resolution));
-        executor.execute(() -> updateCPASeries());
+        executor.execute(this::updateCPASeries);
         executor.execute(() -> updateCPCSeries(resolution));
-        executor.execute(() -> updateCPMSeries());
+        executor.execute(this::updateCPMSeries);
         executor.execute(() -> updateCTRSeries(resolution));
         executor.execute(() -> updateTotalCostSeries(resolution));
         executor.execute(() -> updateClickCountSeries(resolution));
@@ -289,11 +300,8 @@ public class AppController {
 
     private void updateBounceRateSeries(int resolution) {
         in.setBounceRateSeries(ViewDataParser.getBounceRateTimeSeries(resolution, in.getClicks(), in.getInteractions()));
-        Platform.runLater(() -> {
-            bounceRateButton.setDisable(false);
-            //seriesList.add(in.getBounceRateSeries());
-        });
-
+        bounceRateButton.setDisable(false);
+        System.out.println("[Series] bounce rate... done");
     }
 
     private void updateBouncesSeries(int resolution) {
@@ -301,12 +309,14 @@ public class AppController {
                 in.getInteractions().parallelStream()
                         .filter(interaction -> !interaction.isConversion()).map(Interaction::getEntryDate).collect(Collectors.toList())));
         bouncesButton.setDisable(false);
+        System.out.println("[Series] bounces... done");
     }
 
     private void updateClickCountSeries(int resolution) {
         in.setClickCountSeries(ViewDataParser.getCumulativeTimeSeries("Clicks", resolution,
                 in.getClicks().parallelStream().map(Click::getDate).collect(Collectors.toList())));
         clicksButton.setDisable(false);
+        System.out.println("[Series] clicks... done");
     }
 
     private void updateConversionsSeries(int resolution) {
@@ -314,47 +324,55 @@ public class AppController {
                 in.getInteractions().parallelStream()
                         .filter(Interaction::isConversion).map(Interaction::getEntryDate).collect(Collectors.toList())));
         conversionsButton.setDisable(false);
+        System.out.println("[Series] conversions... done");
     }
 
     private void updateCPASeries() {
         in.setCPASeries(ViewDataParser.getCPATimeSeries(in.getImpressions(), in.getClicks(), in.getInteractions()));
         //Platform.runLater(()->seriesList.add(in.getCPASeries()));
         cpaButton.setDisable(false);
+        System.out.println("[Series] CPA... done");
     }
 
     private void updateCPCSeries(int resolution) {
         in.setCPCSeries(ViewDataParser.getCPCTimeSeries(resolution, in.getImpressions(), in.getClicks()));
         //Platform.runLater(()->seriesList.add(in.getCPCSeries()));
         cpcButton.setDisable(false);
+        System.out.println("[Series] CPC... done");
     }
 
     private void updateCPMSeries() {
         in.setCPMSeries(ViewDataParser.getCPMTimeSeries(in.getImpressions(), in.getClicks()));
         //Platform.runLater(()->seriesList.add(in.getCPMSeries()));
         cpmButton.setDisable(false);
+        System.out.println("[Series] CPM... done");
     }
 
     private void updateCTRSeries(int resolution) {
         in.setCtrSeries(ViewDataParser.getTotalCostSeries(resolution, in.getImpressions(), in.getClicks()));
         //Platform.runLater(()->seriesList.add(in.getCtrSeries()));
         ctrButton.setDisable(false);
+        System.out.println("[Series] CTR... done");
     }
 
     private void updateImpressionsSeries(int resolution) {
         in.setImpressionSeries(ViewDataParser.getCumulativeTimeSeries("Impressions", resolution,
                 in.getImpressions().parallelStream().map(Impression::getDate).collect(Collectors.toList())));
         impressionsButton.setDisable(false);
+        System.out.println("[Series] impressions... done");
     }
 
     private void updateTotalCostSeries(int resolution) {
         in.setTotalCostSeries(ViewDataParser.getCTRTimeSeries(resolution, in.getImpressions(), in.getClicks()));
         //Platform.runLater(()->seriesList.add(in.getTotalCostSeries()));
         totalCostButton.setDisable(false);
+        System.out.println("[Series] total cost... done");
     }
 
     private void updateUniquesSeries(int resolution) {
         in.setUniquesSeries(ViewDataParser.getCumulativeTimeSeries("Uniques", resolution,
                 in.getClicks().parallelStream().filter(distinctByKey(Click::getDate)).map(Click::getDate).collect(Collectors.toList())));
         uniquesButton.setDisable(false);
+        System.out.println("[Series] uniques... done");
     }
 }
